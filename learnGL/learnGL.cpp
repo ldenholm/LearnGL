77ffa26e9db55a81abdd460f9cde563b039e1229
Loki <disrute@gmail.com>
2021-02-21 11:16:31 +1100

    Add render commands

--- learnGL/learnGL.cpp
+++ learnGL/learnGL.cpp
@@ -5,6 +5,7 @@
 #include <GLFW/glfw3.h>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow *window);
 
 int main()
 {
@@ -49,11 +50,18 @@ int main()
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
 	// ----------------------------
-	//       Control Loop
+	//        Render Loop
 	// ----------------------------
 
 	while (!glfwWindowShouldClose(window))
 	{
+		// Check if esc pressed.
+		processInput(window);
+
+		// Rendering commands here.
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
@@ -73,4 +81,12 @@ int main()
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
 	glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow *window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+	{
+		glfwSetWindowShouldClose(window, true);
+	}
 }
