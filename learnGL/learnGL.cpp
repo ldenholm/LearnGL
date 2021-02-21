f0cb71121410372633412989f828c14046c687c4
Loki <disrute@gmail.com>
2021-02-21 11:06:28 +1100

    Create a window

--- learnGL/learnGL.cpp
+++ learnGL/learnGL.cpp
@@ -1,21 +1,76 @@
-// learnGL.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+
+// Always include glad before glfw for dependencies
 #include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	// ----------------------------
+	//          SETUP
+	// ----------------------------
+
+   /* - Initialize library.
+	* - Configure to use OpenGL 3.3 (major-minor versions).
+	* - Explicitly declare we will use core-profile, a subset of OpenGL features.
+	*/
+
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	// ----------------------------
+	//       Create Window
+	// ----------------------------
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "Practice Good Learning", NULL, NULL);
+	if (window == NULL)
+	{
+		std::cout << "Failed to create the window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	// Make the context of our window the main context on the current thread.
+	glfwMakeContextCurrent(window);
+
+	// Tell GLAD to load address of the OpenGL function pointers
+    // (which is OS specific), using GetProcAddress.
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+
+	// Callback func to handle window resizing.
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	// ----------------------------
+	//       Control Loop
+	// ----------------------------
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	// ----------------------------
+    //         Terminate
+    // ----------------------------
+
+	glfwTerminate();
+	return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+// ----------------------------
+//       STATIC METHODS
+// ----------------------------
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
