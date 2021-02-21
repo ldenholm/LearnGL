e8395dbcbc0d54f78a17c9d92242395da29f8672
Loki <disrute@gmail.com>
2021-02-21 12:24:11 +1100

    Creating shader

--- learnGL/learnGL.cpp
+++ learnGL/learnGL.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
-
-// Always include glad before glfw for dependencies
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+
+// Always include glad before glfw for dependencies
+
+const char* vertexShaderSource = "#version 330 core\n"
+"layout(location = 0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\0";
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
@@ -46,8 +53,61 @@ int main()
 		return -1;
 	}
 
-	// Callback func to handle window resizing.
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	// ----------------------------
+	//        Vertex Data
+	// ----------------------------
+
+	float vertices[] = {
+		-0.5f, -0.5f, 0.0f, // Left vertex  
+		 0.5f, -0.5f, 0.0f, // Right vertex
+		 0.0f,  0.5f, 0.0f  // Top  vertex
+	};
+
+	// ----------------------------
+    //        Vertex Shader
+    // ----------------------------
+
+	unsigned int vertexShader;
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+
+	// Now attach shader source code to the shader obj and compile the shader.
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+	
+	// Check that the shader compiled successfully.
+	int success;
+	char infoLog[512];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+	
+	if (!success)
+	{
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		std::cout << "Error::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+	};
+
+	// ----------------------------
+	//       Fragment Shader
+	// ----------------------------
+
+
+
+	// First lets create our Vertex Buffer Object.
+	unsigned int VBO;
+	glGenBuffers(1, &VBO);
+	
+	// Bind the buffer to the OpenGL vertex buffer type 'GL_ARRAY_BUFFER'
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+	/* Copy our user-defined data into the currently bound buffer.
+	   Below we pass: Target, Size, Data.
+	   We use GL_STATIC_DRAW because the data does not change, 
+	   if the data were to change frequently we could use GL_DYNAMIC_DRAW
+	   so the GPU will place the data in memory that has faster writes.
+	*/
+
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
 	// ----------------------------
 	//        Render Loop
@@ -66,17 +126,9 @@ int main()
 		glfwPollEvents();
 	}
 
-	// ----------------------------
-    //         Terminate
-    // ----------------------------
-
 	glfwTerminate();
 	return 0;
 }
-
-// ----------------------------
-//       STATIC METHODS
-// ----------------------------
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
